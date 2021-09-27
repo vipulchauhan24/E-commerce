5a9df626588b5b9e157583ee626656c03f83bb97
Vipul Chauhan <vipul9556@gmail.com>
2021-09-27 18:36:12 +0530

    fix backend architecture

--- backend/src/database/connection.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize("e_comm", "root", "vipul",{
-    host: "localhost",
-    dialect : "mysql"
-} );
-
-module.exports = sequelize;
-global.sequelize= sequelize;
