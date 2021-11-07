22f66538058e45b4aa8e9850e15e3b47c7ed1450
vipulchauhan24 <vipul9556@gmail.com>
2021-11-07 13:49:38 +0530

    dovker node version fixed

--- codereact/src/constant.js
+++ codereact/src/constant.js
@@ -1,2 +1,2 @@
-export const REACT_APP_API_URL = "http://13.235.113.167:3001/";
+export const REACT_APP_API_URL = "http://65.0.183.42:3001/";
 // export const REACT_APP_API_URL = "http://localhost:3002/";
